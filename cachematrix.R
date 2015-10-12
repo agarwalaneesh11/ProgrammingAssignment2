b4b00caceb4126c5a229a6221c58f2b57981e29c
unknown <aneesh.agarwal@ASZ-LT-277.AXTRIA.COM>
2015-10-12 13:56:26 +0530

    committed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Program to create a cached matrix and using the same to compute inverse of a matrix
+## which is a costly computation otherwise
 
-## Write a short comment describing this function
+## Creating a cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+  }
 
-}
 
-
-## Write a short comment describing this function
+## Computing inverse of the matrix by caching
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmean()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmean(m)
+    m
 }
